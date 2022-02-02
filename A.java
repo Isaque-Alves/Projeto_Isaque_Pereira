8a9d9dbb748d31410aae30f30db0cb900a36ee75
Isaque Pereira Alves <53536893+Isaque-Alves@users.noreply.github.com>
2022-02-01 21:24:23 -0300

    Update A.java

--- A.java
+++ A.java
@@ -1,4 +1,20 @@
 public class A {
-  int A1;
-  float A2;
+  private int A1;
+  private float A2;
+  
+  public int getA1(){
+    return A1;
+  }
+  
+  public float getA2(){
+    return A2;
+  }
+  
+  public void MA1(){
+    System.out.println("Método MA1");
+  } 
+  
+  public void MA2(){
+    System.out.println("Método MA2");
+  }
 }
