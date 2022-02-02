bcdf4ec98074bc2b2c29089779249a5da3ab298c
Isaque Pereira Alves <53536893+Isaque-Alves@users.noreply.github.com>
2022-02-02 19:06:40 -0300

    Update A.java

--- A.java
+++ A.java
@@ -10,6 +10,14 @@ public class A {
     return A2;
   }
   
+  public void setA1(int A1) {
+    this.A1 = A1;
+  }
+  
+  public void setA2(float A2) {
+    this.A2 = A2;
+  }
+  
   public void MA1(){
     System.out.println("Método MA1");
   } 
