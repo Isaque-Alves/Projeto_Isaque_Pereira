72710666e1d9449826d0deebcfa886ff0e095d00
Isaque Alves <isaquea785@gmail.com>
2022-02-04 14:43:09 -0300

    Criando classe D

---
+++ D.java
@@ -0,0 +1,12 @@
+public class D {
+    private int D1;
+    private float D2;
+    
+    public void MD1() {
+        System.out.println("Método MD1");
+    }
+    
+    public void MD2() {
+        System.out.println("Método MD2");
+    }
+}
